eda6a5e67d9068b72211405cb9f50b9a22748a09
LivingWill <william.hinson@outlook.com>
2021-01-16 23:05:29 -0600

    consolodating code into factory funcs and objects

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-
+//need counter not in global scope (cause instructions) to move if chain for button select
 const Gameboard = () => {
+    
     const gamecontents = [] //prefilled gameboard, render to page
     const winConditions = {
         win1: [0,1,2],
@@ -10,61 +11,55 @@ const Gameboard = () => {
         win6: [2,5,8],
         win7: [0,4,8],
         win8: [3,4,6]
-    }
-    
-    
-}
-
-const score = () => {
-    let count = 0;
-    return () => {
-        console.log(count);
-        count++;
     };
+    let count = 0;
+    return {count}
+};
+const createPlayer = (player, avatar, value, turn, markers) => {
+    this.player = player
+    this.avatar = avatar
+    this.value = value
+    this.turn = turn
+    this.markers = []
+    return {player, avatar, value, turn, markers}
 };
 
-const counter = score();
-
-const playerName = 'echo';
-const icon = 'echo.png';
-
-const createPlayer = ({ playerName, icon }) => ({
-    playerName,
-    icon,
-    setPlayerName (playerName) {
-        this.playerName = playerName;
-        return this;
-    }
-});
-
-createPlayer({playerName: 'Jim', icon: 'ninja.jpg'})
-
-const placeMarker = ({}) => ({
-    
-})
-let turn = false;
-
-const gameClick = document.querySelector('#gamearea').addEventListener('click', (e) => {
+(function() {
+document.querySelector('body').addEventListener('click', (e) => {
     let target = e.target;
-    
     console.log(e)
-    if(target.className == 'markplacement'){
-        let y = target.id
-        let divTarget = document.querySelector(`#${y}`);
-        if(turn == false){
-            let icon = document.createElement('i');
-            icon.classList.add('fas', 'fa-skull-crossbones', 'fa-3x');
-            icon.dataset.id = 'x';
-            divTarget.appendChild(icon);
-            turn = true;
-        } 
-        else if (turn == true){
-            let icon = document.createElement('i')
-            icon.classList.add('fas', 'fa-user-ninja', 'fa-3x')
-            icon.dataset.id = 'o'
-            divTarget.appendChild(icon);
-            turn = false;
-        }
-    }
-})
-    
+    //selects the player faction, disables the button and assigns values via the factory func
+    if(target.localName == 'button'){
+        const player1 = createPlayer('player1', target.id, 0, true)
+        const noPress1 = document.querySelector(`#${target.id}`)
+        noPress1.disabled = true;
+        console.log({player1})
+    } else if (target == 'delete') {
+            const player2 = createPlayer('player2', target.id, 1,false)
+            const noPress2 = document.querySelector(`#${target.id}`)
+            noPress2.disabled = true;
+            console.log({player2})
+    } 
+}
+
+    // if(target.className == 'markplacement' && target.childNodes.length == 0){
+    //     let y = target.id
+    //     console.log(y)
+    //     let divTarget = document.querySelector(`#${y}`);
+    //     if(turn == false){
+    //         let icon = document.createElement('i');
+    //         icon.classList.add('fas', 'fa-skull-crossbones', 'fa-3x');
+    //         icon.dataset.id = 'x';
+    //         divTarget.appendChild(icon);
+    //         turn = true;
+    //     } 
+    //     else if (turn == true){
+    //         let icon = document.createElement('i')
+    //         icon.classList.add('fas', 'fa-user-ninja', 'fa-3x')
+    //         icon.dataset.id = 'o'
+    //         divTarget.appendChild(icon);
+    //         turn = false;
+    //     }
+    // }
+    // })
+)})();
